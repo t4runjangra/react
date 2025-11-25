5003719871347772112fb81071e4104aff55d07a
tarun <tarunjangra1903@gmail.com>
2025-11-25 18:53:03 +0530

    more in react

---
+++ 01-vite/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+   <h2>Tarun Learning React</h2>
+  )
+}
+
+export default App
---
+++ 02-vite/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Card from './components/card';
+function App() {
+
+
+  return (
+    <>
+      <p className='text-center bg-amber-600 h-10 mt-10 '>Tarun here guys!! </p>
+      <Card username = "tarun" />
+    </>
+  )
+}
+
+export default App
